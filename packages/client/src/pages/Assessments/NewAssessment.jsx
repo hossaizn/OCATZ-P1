b43c361e0ed196828d5b0046c68a4971f25b5958
hossaizn <122404015+hossaizn@users.noreply.github.com>
2023-02-01 10:15:06 -0500

    task 1 changes
    i did a bad job at making commits from day 1 so heres a commit of everything i did in task 1

--- packages/client/src/pages/Assessments/NewAssessment.jsx
+++ packages/client/src/pages/Assessments/NewAssessment.jsx
@@ -3,14 +3,132 @@ import { Button, Form } from 'react-bootstrap';
 import { AssessmentService } from '../../services/AssessmentService';
 
 export const NewAssessment = () => {
-
+  const { formState: { errors }, handleSubmit, register } = useForm();
   // create a form that utilizes the "onSubmit" function to send data to
   // packages/client/src/services/AssessmentService.js and then onto the packages/api/src/routes/assessment express API
-  const onSubmit = async (data) => {
-    await AssessmentService.submit(data);
+  const onSubmit = async (formData) => {
+    console.log(formData, `event`);
+    await AssessmentService.submit(formData);
   };
 
-  return <Form>
-    <Button variant="primary" type="submit">Submit</Button>
-  </Form>;
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Label>Cat Behavioral Instrument</Form.Label>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="name" placeholder="Enter Name" {...register(`name`, { required: true })} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicDate">
+        <Form.Label>Date of Birth</Form.Label>
+        <Form.Control type="date" placeholder="Enter date" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPass">
+        <Form.Label>Previous Contact with CAT Judicial System</Form.Label>
+        {[ `radio` ].map((type) =>
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="YES"
+              name="group1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="NO"
+              name="group1"
+              type={type}
+              id={`inline-${type}-2`}
+            />
+          </div>)}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPass">
+        <Form.Label>Physical Altercations with Other Cats</Form.Label>
+        {[ `radio` ].map((type) =>
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="0-3"
+              name="group1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="3+"
+              name="group1"
+              type={type}
+              id={`inline-${type}-2`}
+            />
+          </div>)}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPass">
+        <Form.Label>Physical Altercations With Owner</Form.Label>
+        {[ `radio` ].map((type) =>
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="0-10"
+              name="group1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="10+"
+              name="group1"
+              type={type}
+              id={`inline-${type}-2`}
+            />
+          </div>)}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPass">
+        <Form.Label>Plays Well With Dogs</Form.Label>
+        {[ `radio` ].map((type) =>
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="YES"
+              name="group1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="NO"
+              name="group1"
+              type={type}
+              id={`inline-${type}-2`}
+            />
+          </div>)}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPass">
+        <Form.Label>Hisses At Strangers</Form.Label>
+        {[ `radio` ].map((type) =>
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check
+              inline
+              label="YES"
+              name="group1"
+              type={type}
+              id={`inline-${type}-1`}
+            />
+            <Form.Check
+              inline
+              label="NO"
+              name="group1"
+              type={type}
+              id={`inline-${type}-2`}
+            />
+          </div>)}
+      </Form.Group>
+      <Button variant="primary" type="submit">Submit</Button>
+    </Form>
+  );
 };
